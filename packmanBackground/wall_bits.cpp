78e6b7107a5c28b554be632e564106e9f0f8b231
Jbelly19 <jbelmont19@live.com>
2016-11-10 12:58:58 -0600

    added wall to bit

---
+++ packmanBackground/wall_bits.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "rgbapixel.h"
+#include "png.h"
+#include <map>
+#include <fstream>
+
+using namespace std;
+
+
+int main(){
+
+	fstream myfile;
+	myfile.open("walls.txt", ios::out);
+
+	//Open a new PNG image from the file in.png
+	PNG inputImage("in.png");
+
+	
+	RGBAPixel blue(33,33, 255);
+
+
+
+	for(size_t row = 0; row < inputImage.height(); row++){
+		for(size_t col = 0; col < inputImage.width(); col++){
+			
+			RGBAPixel curr = *(inputImage(col,row));	
+				
+			
+			if(curr != blue){
+
+				myfile <<"1'b,\n";
+
+			}else{
+				myfile <<"0'b,\n";
+			}
+		}
+	}
+							
+
+	myfile.close();
+
+
+	return 0;
+}
